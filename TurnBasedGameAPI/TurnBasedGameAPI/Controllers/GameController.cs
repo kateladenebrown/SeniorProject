052458a91b298a7cae28ebbafb50a375eb744cac
Garrick Evans <its_lightish_red@hotmail.com>
2018-01-22 19:01:00 -0700

    Added User and Game Controller Files and stubbed the GetAll call for the User Controller

---
+++ TurnBasedGameAPI/TurnBasedGameAPI/Controllers/GameController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace TurnBasedGameAPI.Controllers
+{
+    public class GameController : ApiController
+    {
+        // GET: api/Game
+        public IHttpActionResult Index()
+        {
+            return Ok("");
+        }
+    }
+}
---
+++ TurnBasedGameAPI/TurnBasedGameAPI/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace TurnBasedGameAPI.Controllers
+{
+    public class UserController : ApiController
+    {
+        // GET: api/User
+        public IHttpActionResult GetAll()
+        {
+            return Ok("User Controller GetAll API Call");
+        }
+    }
+}
