991c48b6df8b94ea7262a028ed78e769ef93db2e
Stephen <superherostyle@gmail.com>
2018-01-24 13:08:05 -0700

    I added my 3 API call headers and stubs. getPersonalData deleteUser gameCreate

--- TurnBasedGameAPI/TurnBasedGameAPI/Controllers/GameController.cs
+++ TurnBasedGameAPI/TurnBasedGameAPI/Controllers/GameController.cs
@@ -9,6 +9,75 @@ namespace TurnBasedGameAPI.Controllers
 {
     public class GameController : ApiController
     {
+        
+
+        // GET: api/Game/getPeronalData
+        /// <summary>
+        /// Returns all personal data about a user
+        /// coded by Stephen 1/24/18, last updated 1/24/18
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public IHttpActionResult getPersonalDetails( UserId id )
+        {
+            // ask database for personal details from tables matching the user id
+            // returns the data in ?? specific format ?? as object ?? 
+            try
+            {
+                // Obj holder = db.User.where(UserId => id) ;
+
+                // format or return 
+
+            }
+            catch(Exception e){ return Exception("Failure in getPersonalData");}
+
+            return Ok("Game Controller getPersonalDetails API Call");
+        }
+
+        // GET: api/Game/deleteUser
+        /// <summary>
+        /// used to mark users as inactive. Toggles boolean "active" to false
+        /// coded by Stephen 1/24/18, last updated 1/24/18
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public IHttpActionResult deleteUser( UserId id )
+        {
+            // tell database to toggle active to false on user matching 'id'
+            try
+            {
+               // db is the database. varify path to active field
+
+               // db.User.active.where(UserId => id) = false ;
+            }
+            catch(Exception e) { return Exception("failure to deleteUser"); }
+
+            return Ok("Game Controller deleteUser API Call");
+        }
+
+        // GET: api/Game/deleteUser
+        /// <summary>
+        /// creates an instance of game. Uses list<player>
+        /// coded by Stephen 1/24/18, last updated 1/24/18
+        /// </summary>
+        /// <param name="players"></param>
+        /// <returns></returns>
+        public IHttpActionResult gameCreate(List<Player> players)
+        {
+            /* used to instanciate a game instance
+             * args: List of Player, 
+             */
+            try
+            {
+                /* not sure what all we are going to need here just yet.
+                 */
+
+            }
+            catch(Exception e) { return Exception("gameCreate failed to perform as expected. :(   my bad"); }
+            
+            return Ok("Game Controller gameCreate API Call");
+        }
+
         // GET: api/Game/GetMyGames
         /// <summary>
         /// Returns all active User games
