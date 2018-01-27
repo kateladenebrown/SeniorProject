3d7075e1cf0d20f02b71bae6720f27ef3f429b97
Cameron Krome <github@cjkrome.com>
2018-01-27 14:36:41 -0700

    Changes to User/Game controllers (to allow the solution to build), additional comments, enabled help page.

--- TurnBasedGameAPI/TurnBasedGameAPI/Controllers/UserController.cs
+++ TurnBasedGameAPI/TurnBasedGameAPI/Controllers/UserController.cs
@@ -3,141 +3,164 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
-using System.Web.Mvc;
+//using System.Web.Mvc;
 
 namespace TurnBasedGameAPI.Controllers
 {
+    [Authorize]
+    [RoutePrefix("api/User")]
     public class UserController : ApiController
     {
         // GET: api/User/GetAll
-        // Written by Garrick 1/22/18
+        // -Written by Garrick 1/22/18
         /// <summary>
-        /// Returns all Users' usernames
+        /// Retrieves a list of all active user's usernames.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>A list of usernames.</returns>
+        [HttpGet]
+        [Route("GetAll", Name = "Get All Users")]
         public IHttpActionResult GetAll()
         {
             try
             {
                 //var all = db.user.where(active => true)
+                return Ok("User Controller GetAll API Call");
             }
             catch (Exception e)
             {
-                throw new Exception("Error in GetAll API call");
+                return Content(System.Net.HttpStatusCode.InternalServerError, "The server encountered an error while attempting to retrieve the list of users. Please inform the development team.");
             }
-            return Ok("User Controller GetAll API Call");
         }
 
-        //POST: api/User/CreateUser
-        // -Written by Garrick 1/22/18
+        // GET: api/User/GetPersonalDetails
+        // Coded by Stephen 1/24/18
         /// <summary>
-        /// Returns whether or not a user was created successfully.
-        /// Handles account reactivation as well
+        /// Retrieves personal details for the currently logged in user.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>A single User object.</returns>
+        [HttpGet]
+        [Route("GetPersonalDetails", Name = "Get User Personal Details")]
+        public IHttpActionResult GetPersonalDetails()
+        {
+            // ask database for personal details from tables matching the user id
+            // returns the data in ?? specific format ?? as object ?? 
+            try
+            {
+                // Obj holder = db.User.where(UserId => id) ;
+
+                // format or return 
+                return Ok("Game Controller getPersonalDetails API Call");
+            }
+            catch (Exception e)
+            {
+                return Content(System.Net.HttpStatusCode.InternalServerError, "The server was unable to retrieve your personal details. Please inform the development team.");
+            }
+
+        }
+
+        //POST: api/User/Create
+        // -Written by Garrick 1/23/18
+        /// <summary>
+        /// Creates a new user account with the specified details.
+        /// (Handles account reactivation as well.)
+        /// </summary>
+        /// <returns>A message indicating that the user was created/re-activated successfully, or an error otherwise.</returns>
+        [HttpPost]
+        [Route("Create", Name = "Create New User")]
         public IHttpActionResult CreateUser() //(Username, Password, Email, Name, etc)
         {
             try
             {
                 //add user to db
+                return Ok("User Controller CreateUser API Call");
             }
             catch (Exception e)
             {
-                throw new Exception("Error in CreateUser API call");
+                return Content(System.Net.HttpStatusCode.InternalServerError, "The server encountered an error attempting to create the user account. Please inform the development team.");
             }
-            return Ok("User Controller CreateUser API Call");
         }
 
-        // GET: api/Game/GetPeronalData
+        // GET: api/User/{id}
+        // @Michael Case, 1/23/18
         /// <summary>
-        /// Used to gather all personal data about a user.
+        /// Retrieve's the publicly visible information for a single user.
         /// </summary>
-        /// coded by Stephen Bailey 1/24/18
-        /// <param name="id"></param>
-        /// <returns>  Returns all personal data about a user.</returns>
-        public IHttpActionResult GetPersonalDetails(UserId id)
-        {
-            // ask database for personal details from tables matching the user id 
-            try
-            {
-                using (var db = new Game.ENTITIES()) 
-                {
-                    User u = db.Users.Single(u => u.UserId = id);
-                }
-                
-                return Ok( u ); // returns a user object from db matching id.
-            }
-            catch (Exception e) { return Exception("Failure in getPersonalData"); }
-        }
-
-        // GET: api/Game/DeleteUser
-        /// <summary>
-        /// Used to mark users as inactive. Toggles boolean "active" to false
-        /// </summary>
-        /// coded by Stephen Bailey 1/24/18
-        /// <param name="id"></param>
-        /// <returns>IHttp result code</returns>
-        public IHttpActionResult DeleteUser(UserId id)
-        {
-            // tell database to toggle active to false on user matching 'id'
-            try
-            {
-                using (var db = new Game.ENTITIES()) 
-                {
-                    User u = db.Users.Single(u => u.UserId = id);
-                    u.Active = false ;
-                    db.SaveChanges();
-                    return Ok("Game Controller deleteUser API Call"); //  ?? would returning a bool help with this return ??        
-                }
-            }
-            catch (Exception e) { return Exception("failure to deleteUser"); }
-        }
-
-        // GET: api/User
-        /// <summary>
-        /// Returns public information of a single user 
-        /// based on passed in ID.
-        /// @Michael Case, 1/23/18
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        public IHttpActionResult GetByUserID(UserID id) // custom object, int, etc for id? implementation decision for later
+        /// <param name="id">The ID for the user whose details should be returned.</param>
+        /// <returns>A single User object (with only the publicly visible fields populated.</returns>
+        [HttpGet]
+        [Route("{id}", Name = "Get User By ID")]
+        public IHttpActionResult GetByUserID(int id)
         {
             try
             {
                 // This is a stub. It won't work, syntax, names, etc, are wrong,
                 // but should be similar to the final implementation
-                using (var db = new TurnBasedGameAPI.ENTITIES())
-                {
-                    var user = db.users.where(user => user.id);
-                    return Ok("User Controller GetByUserID API Call");
-                }
+
+                // -Cameron: looks good. Commented out for now so that the solution will build.
+
+                //using (var db = new TurnBasedGameAPI.ENTITIES())
+                //{
+                //    var user = db.users.where(user => user.id);
+                //    return Ok("User Controller GetByUserID API Call");
+                //}
+
+                return Ok("User Controller GetByUserID API Call");
             }
             catch (Exception e)
             {
-                return NotFound();
+                return Content(System.Net.HttpStatusCode.InternalServerError, "The server encountered an error attempting to retrieve the user details. Please inform the development team.");
+
+                // -Cameron: NotFound is great to use, but should not be in the outer-most catch block; it should be returned if the database doesn't contain an entry with the ID specified.
+                //return NotFound();
             }
         }
 
-        // PUT: api/User
+        // PUT: api/User/UpdatePassword
+        // @Michael Case, 1/23/18
         /// <summary>
         /// Updates a users password, while verifying that
         /// the new password meets the minimum strength
         /// requirements.
-        /// @Michael Case, 1/23/18
         /// </summary>
-        /// <param name="password"></param>
-        /// <returns></returns>
-        public IHttpActionResult UpdatePassword(String password)
+        /// <param name="password">The new password as a string.</param>
+        /// <returns>A message indicating that the password was updated successfully, or an error otherwise.</returns>
+        [HttpPut]
+        [Route("UpdatePassword", Name = "Update Password")]
+        public IHttpActionResult UpdatePassword(string password)
         {
             // Use another function to check password validity
-            if (CheckPassword(password))
+            if (1 == 1) // (CheckPassword(password))
             {
                 return Ok("User Controller UpdatePassword API Call");
             }
             else
             {
-                return Exception("UpdatePassword call failed");
+                return BadRequest("The password specified does not meet the complexity requirements.");
+            }
+        }
+
+        // DELETE: api/User/Delete
+        // coded by Stephen 1/24/18
+        /// <summary>
+        /// Deactivates the current users account.
+        /// </summary>
+        /// <returns>A message indicating that the account was successfully deactivated, or an error otherwise.</returns>
+        [HttpDelete]
+        [Route("Delete", Name = "Delete User Account")]
+        public IHttpActionResult deleteUser(int id)
+        {
+            // tell database to toggle active to false on user matching 'id'
+            try
+            {
+                // db is the database. varify path to active field
+
+                // db.User.active.where(UserId => id) = false ;
+
+                return Ok("Game Controller deleteUser API Call");
+            }
+            catch (Exception e)
+            {
+                return Content(System.Net.HttpStatusCode.InternalServerError, "The server encountered an error while attempting to deactive the account. Please inform the development team.");
             }
         }
 
