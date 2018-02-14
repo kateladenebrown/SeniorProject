c8956c083bb9e480a9fe77a4a18f075fafe2e77a
James Alberts <albertsjames@hotmail.com>
2018-02-13 18:52:48 -0700

    Started stubbing UpdateGameUserStatus

--- TurnBasedGameAPI/TurnBasedGameAPI/Controllers/GameController.cs
+++ TurnBasedGameAPI/TurnBasedGameAPI/Controllers/GameController.cs
@@ -188,5 +188,83 @@ namespace TurnBasedGameAPI.Controllers
                 return Content(System.Net.HttpStatusCode.InternalServerError, "The server encountered an error when attempting to retrieve the latest game state. Please inform the development team.");
             }
         }
+
+        // POST: api/Game/UpdateGameUserStatus ???
+        // Written by James, 2/13/18
+        /// <summary>
+        /// Determines if status change is valid. If change is valid update game status then handle change accordingly
+        /// </summary>
+        /// <param name="newStatus"></param>
+        /// <param name="gID"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("UpdateGameUserStatus", Name = "Update Game User Status")] // ???
+        public IHttpActionResult UpdateGameUserStatus(int newStatus, int gID)
+        {
+            try
+            {
+                using (var db = new GameEntities())
+                {
+                    List<GameUser> gameUsers = db.Games.Single(g => g.ID == gID).GameUsers.ToList();
+
+                    //Get the list of game states for the id provided and order them by descending.
+                    IQueryable<GameState> gameStatesDesc = db.GameStates.Where(x => x.GameID == gID).OrderByDescending(x => x.TimeStamp);
+
+                    //If game states were found, set the latest one to gameState else set to null.
+                    GameState gameState;
+                    if (gameStatesDesc.Any())
+                    {
+                        gameState = gameStatesDesc.First();
+                    }
+                    else
+                    {
+                        gameState = null;
+                    }
+
+                    // Determine if updating game user status is valid
+                    int oldStatus = db.GameUsers.Single(gu => gu.AspNetUser.UserName == User.Identity.Name).Status;
+                    if(IsValidGUStatusChange(oldStatus, newStatus))
+                    {
+                        // Handle update or possible update to game here
+                        return Ok(); // ???
+                    } else
+                    {
+                        // Correct way to handle this return? NotImplemented status code? something else?
+                        return Content(System.Net.HttpStatusCode.PaymentRequired, "An illegal operation was attempted. Game user status was not updated.");
+                    }
+                }
+            }
+            catch (ArgumentNullException e)
+            {
+                return Content(System.Net.HttpStatusCode.NotFound, "The user who made the call could not be found in the database.");
+            }
+            catch (Exception e)
+            {
+                return Content(System.Net.HttpStatusCode.InternalServerError, "The server encountered an error updating game user status. Please inform the development team.");
+            }
+        }
+
+        // Written by James, 2/13/18
+        /// <summary>
+        /// Determines if status codes are valid to change.
+        /// Status Codes: 1="Pending", 2="Active", 3="Inactive"
+        /// Valid Changes: 1->2 (Accept request), 1->3 (Deny request), 2->3 (Quit game)
+        /// (Possible invalid changes) 2->1, 3->1 (Poss. request sent during active game), 3->2 (Poss. allow user to start midgame)
+        /// </summary>
+        /// <param name="oldStatus"></param>
+        /// <param name="newStatus"></param>
+        /// <returns>True if valid request, false otherwise</returns>
+        private bool IsValidGUStatusChange(int oldStatus, int newStatus)
+        {
+            switch (oldStatus)
+            {
+                case 1:
+                    return (newStatus == 2 || newStatus == 3);
+                case 2:
+                    return newStatus == 3;
+                default:
+                    return false;
+            }
+        }
     }
 }
