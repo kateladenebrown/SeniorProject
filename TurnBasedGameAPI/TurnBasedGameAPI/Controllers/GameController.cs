1eef397fbfee9bee61d68ae8bed9578e376a1585
Garrick Evans <evangarr@isu.edu>
2018-02-24 11:47:50 -0700

    junk commit

--- TurnBasedGameAPI/TurnBasedGameAPI/Controllers/GameController.cs
+++ TurnBasedGameAPI/TurnBasedGameAPI/Controllers/GameController.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
-using GameEF;
 //using System.Web.Mvc;
 
 namespace TurnBasedGameAPI.Controllers
@@ -87,7 +86,7 @@ namespace TurnBasedGameAPI.Controllers
         /// <returns>A list of Game objects.</returns>
         [HttpGet]
         [Route("MyGames", Name = "Get My Games")]
-        public IHttpActionResult GetMyGames(int gameStatus = -1) // string? Gamestatus to check for active vs inactive games
+        public IHttpActionResult GetMyGames(int gameStatus = -1)
         {
             try
             {
