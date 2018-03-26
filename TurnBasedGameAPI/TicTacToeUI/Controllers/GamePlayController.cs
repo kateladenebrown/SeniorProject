df190d8edc41b568c2bef9e54009eafa06853fca
Kate Brown <browkat3@isu.edu>
2018-03-26 08:22:33 -0600

    accept game request  button complete 70% gameboard complete

---
+++ TurnBasedGameAPI/TicTacToeUI/Controllers/GamePlayController.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TicTacToeUI.Models;
+
+namespace TicTacToeUI.Controllers
+{
+    public class GamePlayController : Controller
+    {
+        public ActionResult TTTGamePlay(int gameID, string json)
+        {
+            TTTGameStateModel model = JsonConvert.DeserializeObject<TTTGameStateModel>(json);
+            model.GameId = gameID;
+            return View(model);
+        }
+    }
+}
---
+++ TurnBasedGameAPI/TicTacToeUI/Models/TTTGameStateModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TicTacToeUI.Models
+{
+    public class TTTGameStateModel
+    {
+        public int GameId { get; set; }
+        public string CurrentTurn { get; set; }
+        public Dictionary<string, string> TurnOrder { get; set; }
+        public string[] Grid { get; set; }
+        public string Victor { get; set; }
+        public string CurrentTurnUser { get { return TurnOrder[CurrentTurn]; } }
+    }
+}
