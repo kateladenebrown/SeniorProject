ce2c22cc2e5ae21a83fd72d0ef5a8e1a599d5fdf
Tyler Lancaster <digidragon20@hotmail.com>
2018-03-28 00:23:35 -0600

    Fixing more names

--- TurnBasedGameAPI/TicTacToe/Properties/AssemblyInfo.cs
+++ TurnBasedGameAPI/TicTacToe/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TicTacToe")]
+[assembly: AssemblyTitle("Peril")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TicTacToe")]
+[assembly: AssemblyProduct("Peril")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
