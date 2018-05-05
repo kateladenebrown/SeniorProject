502fd2560e9a5effcd51b0ff1e816f99c1173eb8
Kate Brown <browkat3@isu.edu>
2018-05-05 16:54:04 -0600

    Merge branch 'Kate' into development
    # Conflicts:
    #	TurnBasedGameAPI/TicTacToeUI/Views/Home/Index.cshtml
    #	TurnBasedGameAPI/TicTacToeUI/bin/TicTacToeUI.dll
    #	TurnBasedGameAPI/TicTacToeUI/bin/TicTacToeUI.pdb
    #	TurnBasedGameAPI/TicTacToeUI/obj/Debug/TicTacToeUI.csproj.FileListAbsolute.txt
    #	TurnBasedGameAPI/TicTacToeUI/obj/Debug/TicTacToeUI.dll
    #	TurnBasedGameAPI/TicTacToeUI/obj/Debug/TicTacToeUI.pdb
    #	TurnBasedGameAPI/TurnBasedGameAPI/Web.config
    #	TurnBasedGameAPI/TurnBasedGameAPI/bin/GameEF.dll
    #	TurnBasedGameAPI/TurnBasedGameAPI/bin/GameEF.pdb
    #	TurnBasedGameAPI/TurnBasedGameAPI/bin/TurnBasedGameAPI.dll
    #	TurnBasedGameAPI/TurnBasedGameAPI/bin/TurnBasedGameAPI.pdb

---
+++ TurnBasedGameAPI/TicTacToeUI/Controllers/GamePlayController.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TicTacToeUI.Models;
+
+namespace TicTacToeUI.Controllers
+{
+    public class GamePlayController : Controller
+    {
+        public ActionResult TTTGamePlay(int gameID)
+        {
+            return View(gameID);
+        }
+    }
+}
